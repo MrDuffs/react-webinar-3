5b42cb0953151d83cd7ca34e3d84a8b7657dc863
MrDuffs <chulkov.i@yandex.ru>
2023-05-25 16:43:21 +0300

    add pagination to main and callback for changing page

--- src/app/main/index.js
+++ src/app/main/index.js
@@ -6,6 +6,7 @@ import BasketTool from "../../components/basket-tool";
 import List from "../../components/list";
 import useStore from "../../store/use-store";
 import useSelector from "../../store/use-selector";
+import Pagination from '../../components/pagination';
 
 function Main() {
 
@@ -17,6 +18,8 @@ function Main() {
 
   const select = useSelector(state => ({
     list: state.catalog.list,
+    currentPage: state.catalog.currentPage,
+    totalPages: state.catalog.totalPages,
     amount: state.basket.amount,
     sum: state.basket.sum
   }));
@@ -26,6 +29,8 @@ function Main() {
     addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store]),
     // Открытие модалки корзины
     openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store]),
+    // Подгрузка новых данных при смене страницы
+    changePage: useCallback((page) => store.actions.catalog.load(page), [])
   }
 
   const renders = {
@@ -40,6 +45,11 @@ function Main() {
       <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount}
                   sum={select.sum}/>
       <List list={select.list} renderItem={renders.item}/>
+      <Pagination
+        currentPage={select.currentPage}
+        totalPages={select.totalPages}
+        onChangePage={callbacks.changePage}
+      />
     </PageLayout>
 
   );
