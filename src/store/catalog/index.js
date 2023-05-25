80de8b9ccbd582d383c1eece79e24d14a37ae461
MrDuffs <chulkov.i@yandex.ru>
2023-05-25 16:44:23 +0300

    edit load method

--- src/store/catalog/index.js
+++ src/store/catalog/index.js
@@ -10,16 +10,22 @@ class Catalog extends StoreModule {
 
   initState() {
     return {
-      list: []
+      list: [],
+      currentPage: 1
     }
   }
 
-  async load() {
-    const response = await fetch('/api/v1/articles');
+  async load(page) {
+    const currentPage = page ? page : this.getState().currentPage;
+    const skip = (currentPage - 1) *  10;
+
+    const response = await fetch(`/api/v1/articles?limit=10&skip=${skip}&fields=items(_id, title, price),count`);
     const json = await response.json();
     this.setState({
-       ...this.getState(),
-       list: json.result.items
+      ...this.getState(),
+      list: json.result.items,
+      currentPage,
+      totalPages: Math.ceil(json.result.count / 10)
     }, 'Загружены товары из АПИ');
   }
 }
