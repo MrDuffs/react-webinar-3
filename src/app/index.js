a38950679d402b133b2fdd4856e7ceb5bff2e388
MrDuffs <chulkov.i@yandex.ru>
2023-06-03 21:34:17 +0300

    add routes for login and profile pages

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,20 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import useSelector from "../hooks/use-selector";
 import Main from "./main";
 import Basket from "./basket";
 import Article from "./article";
 import Login from "./login";
+import Profile from './profile';
 
 /**
  * Приложение
  * @returns {React.ReactElement}
  */
 function App() {
+
+  const select = useSelector(state => ({
+    userData: state.login.userData,
+  }));
 
   const activeModal = useSelector(state => state.modals.name);
 
@@ -18,7 +23,8 @@ function App() {
       <Routes>
         <Route path={''} element={<Main/>}/>
         <Route path={'/articles/:id'} element={<Article/>}/>
-        <Route path={'/login'} element={<Login/>} />
+        <Route path={'/login'} element={select.userData ? <Navigate to='/profile'/> : <Login/>} />
+        <Route path={'/profile'} element={!select.userData ? <Navigate to='/login'/> : <Profile/>} />
       </Routes>
 
       {activeModal === 'basket' && <Basket/>}
